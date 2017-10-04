8d45b135f842e4515297b47fa9459466a34ac8b9
Hank Andre <hankandre@gmail.com>
2017-10-04 05:03:12 -0500

    removed unneccesary modules

--- presentation/index.js
+++ presentation/index.js
@@ -1,13 +1,8 @@
 // Import React
 import React from 'react';
 import {
-  BlockQuote,
-  Cite,
   Deck,
   Heading,
-  List,
-  ListItem,
-  Quote,
   Slide,
   Text
 } from 'spectacle';
@@ -58,43 +53,6 @@ export default class Presentation extends React.Component {
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
             https://🖥🚀.ws
           </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem> Item 1 </ListItem> <ListItem> Item 2 </ListItem>
-            <ListItem> Item 3 </ListItem> <ListItem> Item 4 </ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote> Example Quote </Quote> <Cite> Author </Cite>
-          </BlockQuote>
         </Slide>
       </Deck>
     );
